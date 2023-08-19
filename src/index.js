5c176bac2b2bbe2e97bf98537c5b95e4eb040e59
anicegbem <matthewdlama4@gmail.com>
2023-08-19 21:54:31 +0100

    add button to header

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Hello");
+import header from "./page.js";
+
+function initializeWebsite() {
+    const content = document.getElementById("content");
+    const body = document.body
+    content.appendChild(header());
+    body.appendChild(content);
+    
+    return body;
+}
+
+initializeWebsite();
--- src/page.js
+++ src/page.js
@@ -1,3 +1,41 @@
+function createHeader() {
+    const header = document.createElement('header');
+    const h1 = document.createElement('h1');
+    h1.innerText = "Igyo-Dondo";
+    header.appendChild(h1);
+    header.appendChild(createNav());
+
+    return header
+
+}
+
+function createNav() {
+    const nav = document.createElement('nav');
+    const ul = document.createElement('ul');
+    
+    for(let i = 0; i < 3; i++) {
+        const btn = document.createElement('button');
+        btn.setAttribute('type', 'button');
+        if(i === 0) {
+            btn.innerText = "Home";
+            btn.setAttribute('id', 'Home');
+        } else if (i === 1) {
+            btn.innerText = "Menu";
+            btn.setAttribute('id', 'Menu');
+        } else if (i === 2) {
+            btn.innerText = "Contact";
+            btn.setAttribute('id', 'Contact');
+        }
+        ul.appendChild(btn);
+
+    }
+    nav.appendChild(ul);
+    return nav;
+}
+
+export default createHeader;
+
+
 function loadPage() {
     const content = document.getElementById("content");
     const nav = document.createElement('nav');
